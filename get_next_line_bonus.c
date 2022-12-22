3053aa3b23801aec1887a46b5f4c10a659fe4c13
Yassine-Zaazaa <Yassine.zaazaa@outlook.com>
2022-12-22 15:24:04 +0100

    Bonus

--- get_next_line_bonus.c
+++ get_next_line_bonus.c
@@ -7,31 +7,27 @@ char	*get_next_line(int fd)
 	static char	str[FOPEN_MAX][BUFFER_SIZE + 1];
 
 	i = 1;
-	if(fd < 0 || BUFFER_SIZE < 1)
-		return (0);
-	line = ft_strjoin(0, str);
-	if(check_str(str))
+	if(fd < 0 || fd > FOPEN_MAX || BUFFER_SIZE < 1)
+		return (NULL);
+	line = ft_strjoin(0, str[fd]);
+	if(check_str(str[fd]))
 		return (line);
 	while(i > 0)
 	{
-		i = read(fd, str, BUFFER_SIZE);
+		i = read(fd, str[fd], BUFFER_SIZE);
 		if(i == -1)
-			return (ft_free(line));
+		{
+			free(line);
+			return (NULL);
+		}
 		if(i == 0 && line[0] == '\0')
-			return (ft_free(line));
-		line = ft_strjoin(line, str);
-		if(check_str(str))
+		{
+			free(line);
+			return (NULL);
+		}	
+		line = ft_strjoin(line, str[fd]);
+		if(check_str(str[fd]))
 			break;
 	}
 	return (line);
 }
-/*
-int	main()
-{
-	int	fd = open("4lines.txt", O_RDONLY);
-
-	printf("%s\n", get_next_line(fd));
-	printf("%s\n", get_next_line(fd));
-	printf("%s\n", get_next_line(fd));
-	return (0);
-}*/
--- get_next_line_bonus.h
+++ get_next_line_bonus.h
@@ -1,5 +1,5 @@
-#ifndef GET_NEXT_LINE_H
-#	define GET_NEXT_LINE_H
+#ifndef GET_NEXT_LINE_BONUS_H
+#	define GET_NEXT_LINE_BONUS_H
 #	include <stdio.h>
 #	include <stdlib.h>
 #	include <unistd.h>
@@ -9,7 +9,6 @@
 
 size_t	ft_strlen(const char *s);
 char	*ft_strjoin(char *s1, char *s2);
-char	*ft_free(char *s);
 int	check_str(char *str);
 char	*get_next_line(int fd);
 #endif
